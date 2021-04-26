b52aeef9de43faa4da5f5c361d018dc273c9e91e
kyulgon <57319096+kyulgon@users.noreply.github.com>
2021-04-26 18:55:34 +0900

    Quest_UI 구현

---
+++ Quest3_UI/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button button;
+    [SerializeField]
+    private Text text;
+
+    public GameObject menuOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(() =>
+        {
+            text.text = "버튼 클릭!";
+        });
+    }
+
+    void Update()
+    {
+        if(menuOffset != null)
+        {
+            this.transform.position = menuOffset.transform.position;
+            this.transform.rotation = menuOffset.transform.rotation;
+
+        }    
+    }
+}
