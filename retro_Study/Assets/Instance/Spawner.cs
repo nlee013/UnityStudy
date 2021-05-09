a90c121f0c6a00fe8f27373a63c85147436b8880
unknown <kyulgon@naver.com>
2021-05-09 22:46:34 +0900

    retro Class_ List, Static, overload, instance 예제

---
+++ retro_Study/Assets/Instance/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject target;
+    public Transform spawnPosition;
+
+    void Start()
+    {
+        GameObject instance = Instantiate(target, spawnPosition.position, spawnPosition.rotation);
+
+        instance.GetComponent<Rigidbody>().AddForce(0, 1000, 0);
+        Debug.Log(instance.name);
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ retro_Study/Assets/List/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public List<int> scores = new List<int>();
+
+
+    //void Update()
+    //{
+    //    if(Input.GetMouseButtonDown(0))
+    //    {
+    //        int randomNumer = Random.Range(0, 100);
+    //        scores.Add(randomNumer);
+    //    }
+
+    //    if(Input.GetMouseButtonDown(1))
+    //    {
+    //        scores.RemoveAt(3);
+    //    }
+    //}
+
+    private void Start()
+    {
+        //int score0 = 45;
+        //int score1 = 20;
+        //int score2 = 70;
+
+        //scores.Add(score0);
+        //scores.Add(score1);
+        //scores.Add(score2);
+
+        //scores.Remove(60);
+
+        //Debug.Log(scores[1]);
+
+        while(scores.Count > 0)
+        {
+            scores.RemoveAt(0);
+        }
+
+    }
+}
